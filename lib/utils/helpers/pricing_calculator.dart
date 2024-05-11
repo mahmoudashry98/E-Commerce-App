0d4d167d880396f1042e942e1307d01184c8a491
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2024-05-11 21:06:29 +0300

    add helpers and pricing calculator

---
+++ lib/utils/helpers/pricing_calculator.dart
@@ -0,0 +1,33 @@
+class PricingCalculatorApp {
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCost = getShippingCost(location);
+    double totalPrice = productPrice + taxAmount + shippingCost;
+    return totalPrice;
+  }
+
+  static double calculateDiscountedPrice(double price, double discount) {
+    return price - (price * discount) / 100;
+  }
+
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  static String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    return 0.10;
+  }
+}
+
+double getShippingCost(String location) {
+  return 5.00;
+}
